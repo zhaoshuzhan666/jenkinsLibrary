533750bf1f2b6c49c7ad0e3e9e1114ecc290e899
sheyuzsz <zsz19901116@163.com>
2022-09-18 06:54:33 +0000

    update src/org/devops/build.groovy.
    Signed-off-by: sheyuzsz <zsz19901116@163.com>

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -2,7 +2,7 @@ package org.devops
 
     //构建类型
     def Build(buildType,buildShell){
-        def buildTools = ["mvn":"M2","ant":"ANT","gradle":"GRADLE","npm":"NPM"]
+        def buildTools = ["mvn":"m2","ant":"ANT","gradle":"GRADLE","npm":"NPM"]
         println("当前选择的构建类型为 ${buildType}")
         sh "${buildHome}/bin/${buildType} ${buildShell}"
     
