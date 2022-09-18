dfdcda3a486f051cc7031ed88930411473b4ec3d
sheyuzsz <zsz19901116@163.com>
2022-09-18 06:46:32 +0000

    update src/org/devops/tools.groovy.
    Signed-off-by: sheyuzsz <zsz19901116@163.com>

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -10,7 +10,7 @@ def PrintMes(value,color){
         println(colors[color])
     }
     //构建类型
-    dev Build(buildType,buildShell){
+    def Build(buildType,buildShell){
         def buildTools = ["mvn":"M2","ant":"ANT","gradle":"GRADLE","npm":"NPM"]
         println("当前选择的构建类型为 ${buildType}")
         sh "${buildHome}/bin/${buildType} ${buildShell}"
