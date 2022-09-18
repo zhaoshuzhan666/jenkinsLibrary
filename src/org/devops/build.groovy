66c0763afa720beea7a001578d9978ad670ddc3f
sheyuzsz <zsz19901116@163.com>
2022-09-18 06:52:49 +0000

    update src/org/devops/build.groovy.
    Signed-off-by: sheyuzsz <zsz19901116@163.com>

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -5,5 +5,5 @@ package org.devops
         def buildTools = ["mvn":"M2","ant":"ANT","gradle":"GRADLE","npm":"NPM"]
         println("当前选择的构建类型为 ${buildType}")
         sh "${buildHome}/bin/${buildType} ${buildShell}"
-    }
+    
 }
